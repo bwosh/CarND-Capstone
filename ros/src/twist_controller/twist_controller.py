f92d46fefc74c3f14a296b7b2121a295d245c0b2
Błażej Matuszewski <blazej.matuszewski@intive.com>
2020-01-17 13:55:53 +0100

    PID params taken from other parameters search

--- ros/src/twist_controller/twist_controller.py
+++ ros/src/twist_controller/twist_controller.py
@@ -15,9 +15,9 @@ class Controller(object):
                  wheel_radius, wheel_base, steer_ratio, max_lat_accel, max_steer_angle):
         self.yaw_controller = YawController(wheel_base, steer_ratio, 0.1, max_lat_accel, max_steer_angle)
         
-        kp = 0.3
-        ki = 0.1
-        kd = 0
+        kp = 0.18
+        ki = 0.00001
+        kd = 3.9
         mn = 0.1
         mx = 0.2
 
